a81e3a01d0c44ad2610fe7ece73a9aa29f592a21
yuk1teru <artemk0b2396@gmail.com>
2021-06-14 23:23:08 +0300

    request + footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,33 @@
-$(function () {});
+$(function () {
+  const swiper = new Swiper('.swiper-container', {
+    // loop: true,
+    spaceBetween: 100,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  // Кнопка показати більше
+
+  $('.portfolio__inner .show-more').on('click', function (e) {
+    var $this = $(this);
+    $this.find('span').toggleClass('hidden');
+    $('.reviews-item:gt(0)').toggle();
+  });
+
+  $('.portfolio__inner .show-more').on('click', function (e) {
+    var $wrapper = $('.swiper-wrapper');
+    $wrapper.toggleClass('show-content');
+  });
+  //
+  // if ($('.product-item').length > 3) {
+  //     $('.product-item:gt(2)').hide().removeClass('aos-init aos-animate');
+  //     $('.show-more').show();
+  // }
+  // $('.catalog-wrapper .show-more').on('click', function (e) {
+  //     var $this = $(this);
+  //     $this.find('span').toggleClass('hidden');
+  //     $('.product-item:gt(2)').toggle();
+});
