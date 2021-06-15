ef2a10e9aadd2359faec24335b928f0898bcddb1
yuk1teru <artemk0b2396@gmail.com>
2021-06-15 13:49:39 +0300

    Закончил desktop

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,41 @@ $(function () {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  //скрол меню
+
+  $('#menu-order').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: $('#order').offset().top,
+      },
+      1000,
+    );
+  });
+  $('#menu-hero').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: $('#hero').offset().top,
+      },
+      1000,
+    );
+  });
+  $('#menu-portfolio').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: $('#portfolio').offset().top,
+      },
+      1000,
+    );
+  });
+  $('#menu-contacts').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: $('#contacts').offset().top,
+      },
+      1000,
+    );
+  });
   // Кнопка показати більше
 
   $('.portfolio__inner .show-more').on('click', function (e) {
@@ -21,13 +56,25 @@ $(function () {
     var $wrapper = $('.swiper-wrapper');
     $wrapper.toggleClass('show-content');
   });
-  //
-  // if ($('.product-item').length > 3) {
-  //     $('.product-item:gt(2)').hide().removeClass('aos-init aos-animate');
-  //     $('.show-more').show();
-  // }
-  // $('.catalog-wrapper .show-more').on('click', function (e) {
-  //     var $this = $(this);
-  //     $this.find('span').toggleClass('hidden');
-  //     $('.product-item:gt(2)').toggle();
+
+  //Форми
+  $('.form__close').on('click', function (e) {
+    var $body = $('body');
+    var $overlay = $('.overlay');
+    $overlay.addClass('hide');
+    $body.removeClass('form-open');
+  });
+
+  $('.btn-cost').on('click', function (e) {
+    var $body = $('body');
+    var $overlayC = $('.overlay-cost');
+    $overlayC.removeClass('hide');
+    $body.addClass('form-open');
+  });
+  $('.btn-request').on('click', function (e) {
+    var $body = $('body');
+    var $overlayR = $('.overlay-request');
+    $overlayR.removeClass('hide');
+    $body.addClass('form-open');
+  });
 });
