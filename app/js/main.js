f646d25cf570125d755e877a1cb4dcb3472a93c3
yuk1teru <artemk0b2396@gmail.com>
2021-06-17 15:26:27 +0300

    Предвнедрение webP

--- app/js/main.js
+++ app/js/main.js
@@ -2,21 +2,21 @@ $(function () {
   const swiper = new Swiper('.swiper-container', {
     // loop: true,
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 50,
 
-    // breakpoints: {
-    //   // when window width is >= 320px
-    //   0: {
-    //     slidesPerView: 1,
-    //   },
-    //   // when window width is >= 480px
-    //   520: {
-    //     slidesPerView: 1,
-    //   },
-    //   1065: {
-    //     slidesPerView: 3,
-    //   },
-    // },
+    breakpoints: {
+      // when window width is >= 320px
+      0: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      520: {
+        slidesPerView: 1,
+      },
+      1065: {
+        slidesPerView: 3,
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
