5a3d78e16faa5c99bc016f686cad42b59205f495
yuk1teru <artemk0b2396@gmail.com>
2021-06-18 13:37:46 +0300

    title

--- app/js/main.js
+++ app/js/main.js
@@ -61,7 +61,7 @@ $(function () {
   $('#hero__arrow-down').click(function () {
     $('html, body').animate(
       {
-        scrollTop: $('#portfolio').offset().top,
+        scrollTop: $('#request').offset().top,
       },
       1000,
     );
